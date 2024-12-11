4878e9e251e1e241007f60e2b73df928774dd369
Danae Muñoz Opitz <dana.munozo@duocuc.cl>
2024-12-11 11:20:06 -0300

    sa

--- run.bat
+++ run.bat
@@ -1,3 +1,3 @@
 rem ionic cap run android -l --external --host=192.168.100.37 --port=8100 --target=R58T614VCGP
 rem ionic cap run android -l --external --host=192.168.1.9 --port=8101 --target=5hmnvsxc5h55vklj
-ionic cap run android -l --external --host=192.168.65.11 --port=8101 --target=5hmnvsxc5h55vklj 
+ionic cap run android -l --external --host=192.168.193.11 --port=8101 --target=5hmnvsxc5h55vklj 
